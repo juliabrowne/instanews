d08cc7c14cb304c4169a9581f8389a1e72172029
Julia Browne <julia.browne@live.ca>
2018-07-19 15:54:41 -0700

    Edit JS file. Add article images as background images.

--- js/index.js
+++ js/index.js
@@ -2,69 +2,86 @@ $(document).ready(function () {
 
 
 
-      $('#sections').on('change', function () {
+  $('#sections').on('change', function () {
 
-        //try appending loading gif
+      //try appending loading gif
 
-        var selectedStory = $('#sections').val();
-        console.log(selectedStory);
-
-
-        $(".article-grid").empty();
+      var selectedStory = $('#sections').val();
+      console.log(selectedStory);
 
 
-
-        //url for our API request
-        var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectedStory + '.json';
-        url +=
-          '?' + $.param({
-            'api-key': '7c08d5d706ea4a07a18b431e666a976f'
-          });
-
-        //ajax request
-        $.ajax({
-            url: url,
-            method: 'GET'
-          })
-          .done(function (data) {
-            console.log(data.results);
-
-            $.each(data.results, function (key, value) {
-              console.log(value.abstract);
-
-              var html = '<div class="new-cell">';
-              html += '<a target="_blank" href=' + value.url + '>'
-              html += '<img class="news-img" src=' + value.multimedia[4].url + '>';
-              html += '<p>' + value.abstract + '</p>' + '</a>';
-              html += '</div>';
+      $(".article-grid").empty();
 
 
-              $('.article-grid').append(html);
-              // var html = class name for grid
-              // html += "<p>"  value from before "</p>"
-              // html += past value
-              // $('.article-grid').append('<p>' + value.abstract + '</p>');
 
-            })
+      //url for our API request
+      var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectedStory + '.json';
+      url +=
+        '?' + $.param({
+          'api-key': '7c08d5d706ea4a07a18b431e666a976f'
+        });
+
+      //ajax request
+      $.ajax({
+          url: url,
+          method: 'GET'
+        })
+        .done(function (data) {
+          var filterResults = data.results.filter(function(result) {
+            return results.multimedia;
+          }).slice(0, 12);
+
+
+
+          $.each(data.results, function (key, value) {
+
+
+            // var html = '<div class="new-cell">';
+            // html += '<a target="_blank" href=' + value.url + '>'
+            // html += '<img class="news-img" src=' + value.multimedia[4].url + '>';
+            // html += '<p>' + value.abstract + '</p>' + '</a>';
+            // html += '</div>';
+
+
+
+            // var html = '<div class="new-cell">';
+            // var bgImg = $('.bg-img').css('background-image' + value.multimedia[4].url);
+
+            var artLink = value.url;
+            var artImg = value.multimedia[4].url;
+            var artText = value.abstract;
+            var html = '<div class="new-cell">';
+
+            html += '<a href=' + artLink + '>';
+            html += '<div class="bg-img" style="background-image: url(' + artImg + ')">';
+            html += '<div class="art-text">';
+            html += '<p>' + artText + '</p>' + '</div>' + '</a>';
+            html += '</div>';
+
+
           })
         })
+    })
 
 
 
-        // var resultsArray = data.results;
+    //var resultsArray = data.results;
 
-        //try using $.each to look through the data (ex: resultsArray) and check out the array in data called //results and try appending the output to your html
-
-        .fail(function (err) {
-            throw err;
-          })
+    // try using $.each to look through the data (ex: resultsArray) and check out the    // array in data called 
+    //results and try appending the output to your html
 
 
-          .always(function (err) {
-            //try removing or detaching the loading gif
+
+    .fail(function (err) {
+      throw err;
+    })
 
 
-          }); // #sections change event
+    .always(function (err) {
+      //try removing or detaching the loading gif
 
 
-      }); //end doc ready
+    }); // #sections change event
+
+
+}); 
