6c6bb8c098b46612a844c876f64eb07508f61493
Julia Browne <julia.browne@live.ca>
2018-07-21 23:02:29 -0700

    Center article images using Sass. Fix image height and font size.

--- js/index.js
+++ js/index.js
@@ -1,84 +1,66 @@
 $(document).ready(function () {
 
+// $(window).resize(function () {
+// console.log($(window).width()); 
+// }); 
+// add class to header
+// to reduce size of logo
 
-  $(window).resize(function() {
-    console.log($(window).width());
-  });
+$('#sections').on('change', function () {
 
+var loader = '<div id="loading-gif">'; 
+loader += '<img src="../images/ajax-loader.gif" alt="Loading Gif">'; 
+loader += '</div>'; 
 
-  $('#sections').on('change', function () {
+var selectedStory = $('#sections').val(); 
 
-    var loader = '<div id="loading-gif">';
-    loader += '<img src="../images/ajax-loader.gif" alt="Loading Gif">';
-    loader += '</div>';
+$('.article-grid').empty(); 
+$('.article-grid').append(loader); 
 
-    
-    //try appending loading gif
-    
-    // add class to header
-    // to reduce size of logo
-    
-    
-    
-    var selectedStory = $('#sections').val();
-    
-    $('.article-grid').append(loader);
-    $('.article-grid').empty();
- 
+//url for API request
+var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectedStory + '.json';
+url += 
+'?' + $.param( {
+'api-key':'7c08d5d706ea4a07a18b431e666a976f'
+}); 
 
+//ajax request
+$.ajax( {
+url:url, 
+method:'GET'
+})
+.done(function (data) {
+// data.results are filtered to only show articles that have images
+var filterResults = data.results.filter(function (result) {
+return result.multimedia; 
+}).slice(0, 12); 
 
-    //url for our API request
-    var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectedStory + '.json';
-    url +=
-      '?' + $.param({
-        'api-key': '7c08d5d706ea4a07a18b431e666a976f'
-      });
+$.each(filterResults, function (key, value) {
+var artLink = value.url; 
+var artImg = value.multimedia[4].url; 
+var artText = value.abstract; 
+var html = '<div class="new-cell">'; 
 
-    //ajax request
-    $.ajax({
-        url: url,
-        method: 'GET'
-      })
-      .done(function (data) {
-
-        /**
-         * Filter data.results to only return articles if they have images
-         */
-        var filterResults = data.results.filter(function (result) {
-          return result.multimedia;
-        }).slice(0, 12);
-
-        console.log(filterResults);
-
-        $.each(filterResults, function (key, value) {
-          var artLink = value.url;
-          var artImg = value.multimedia[4].url;
-          var artText = value.abstract;
-          var html = '<div class="new-cell">';
-
-          html += '<a href=' + artLink + '>';
-          html += '<div class="bg-img" style="background-image: url(' + artImg + '); background-size: cover;">';
-          html += '<div class="art-text">';
-          html += '<p>' + artText + '</p>' + '</div>' + '</a>';
-          html += '</div>';
-
-          $('.article-grid').remove('#loading-gif');
-          $('.article-grid').append(html);
-
-        }); // end of .each
-      })
-      .fail(function (err) {
-        alert("an error has occured, please try again");
-        console.log(err);
-      })
-      .always(function () {
-        $('#loading-gif').remove();
-
-        //try removing or detaching the loading gif
-      }); // end of .ajax
+html += '<a href=' + artLink + '>'; 
+html += '<div class="bg-img" style="background-image: url(' + artImg + '); background-size: cover;">'; 
+html += '<div class="art-text">'; 
+html += '<p>' + artText + '</p>' + '</div>' + '</a>'; 
+html += '</div>'; 
 
 
-  }); // #sections change event
+$('.article-grid').remove('#loading-gif'); 
+$('.article-grid').append(html); 
 
+}); // end of .each
+})
+.fail(function (err) {
+alert('Oops, something went wrong. Please try again!'); 
+console.log(err); 
+})
+.always(function () {
+$('#loading-gif').remove(); 
+}); // end of .ajax
+
+}); // #sections change event
 
 }); // end of doc ready
